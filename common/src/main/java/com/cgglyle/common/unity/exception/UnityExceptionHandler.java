e700026972e99cf5ee7b23b41e76d1ab1b127398
Lyle <86522347+cgglyle@users.noreply.github.com>
2022-08-14 15:31:41 +0800

    更新：统一日志管理HTTP状态码系统

--- common/src/main/java/com/cgglyle/common/unity/exception/UnityExceptionHandler.java
+++ common/src/main/java/com/cgglyle/common/unity/exception/UnityExceptionHandler.java
@@ -4,8 +4,10 @@ import com.cgglyle.common.unity.status.ClientErrorCode;
 import com.cgglyle.common.unity.status.ResultVo;
 import com.cgglyle.common.unity.status.SystemErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
@@ -25,7 +27,9 @@ public class UnityExceptionHandler {
      * <p>
      * 没有返回枚举类中的异常信息
      */
+
     @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResultVo<String> exceptionHandler(Exception e){
         log.error("未定义异常", e);
         return ResultVo.error(SystemErrorCode.SYSTEM_ERROR);
@@ -37,6 +41,7 @@ public class UnityExceptionHandler {
      * 除以零异常
      */
     @ExceptionHandler(value = ArithmeticException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ResultVo<String> exceptionHandler(ArithmeticException e){
         log.error(SystemErrorCode.DIVISION_ZERO_ERROR.getMsg(), e);
         return ResultVo.error(SystemErrorCode.DIVISION_ZERO_ERROR);
@@ -49,6 +54,7 @@ public class UnityExceptionHandler {
      * @return 返回错误代码，错误信息，不包含数据
      */
     @ExceptionHandler(value = ClientException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResultVo<String> exceptionHandler(ClientException e) {
         log.error(ClientErrorCode.USERNAME_PASSWORD_ERROR.getMsg(), e);
         return ResultVo.error(e.getErrorCode(), e.getErrorMassage());
@@ -62,6 +68,7 @@ public class UnityExceptionHandler {
      * @return 返回错误代码，错误信息，错误位置
      */
     @ExceptionHandler(value = BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResultVo<String> exceptionHandler(BindException e) {
         log.error(ClientErrorCode.DATA_ERROR.getMsg(), e);
         return ResultVo.error(ClientErrorCode.DATA_ERROR,
@@ -76,6 +83,7 @@ public class UnityExceptionHandler {
      * @return 返回错误代码，错误信息，错误位置
      */
     @ExceptionHandler(value = ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResultVo<String> exceptionHandler(ConstraintViolationException e) {
         log.error(ClientErrorCode.DATA_ERROR.getMsg(), e);
         return ResultVo.error(ClientErrorCode.DATA_ERROR, e);
